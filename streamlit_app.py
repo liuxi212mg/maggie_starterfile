e48171e7d3c1c9d25648f358fd9d943e0387c91a
liuxi212mg <maggie_x_liu@otpp.com>
2024-07-31 15:56:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+pip install streamlit-navigation-bar
+
+from streamlit_navigation_bar import st_navbar
+
+page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+st.write(page)
 
 st.title('testingmg')
-
 st.write('Hello world!')
