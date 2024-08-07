0b6aeab2194c83ab08c8a6762f42d118b138b14b
liuxi212mg <maggie_x_liu@otpp.com>
2024-08-07 10:06:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ st.checkbox('Check me out')
 st.radio('Pick one:', ['nose','ear'])
 st.selectbox('Select', [1,2,3])
 st.multiselect('Multiselect', [1,2,3])
+
+>>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+>>> tab1.write("this is tab 1")
+>>> tab2.write("this is tab 2")
