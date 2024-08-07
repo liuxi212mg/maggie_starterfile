3c7cb79b7ee26c8a9a0fb72a5de022f9dca84e12
liuxi212mg <maggie_x_liu@otpp.com>
2024-08-07 09:58:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title('testingmg')
 st.write('Hello world!')
+
+st.button('Hit me')
+st.data_editor('Edit data', data)
+st.checkbox('Check me out')
+st.radio('Pick one:', ['nose','ear'])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
