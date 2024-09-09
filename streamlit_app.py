a4303698e4414e21c35ccd8538113bc461f2fb02
liuxi212mg <maggie_x_liu@otpp.com>
2024-09-09 16:30:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,6 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title('testingmg')
 st.write('Hello world!')
 
-st.button('Hit me')
-st.checkbox('Check me out')
-st.radio('Pick one:', ['nose','ear'])
-st.selectbox('Select', [1,2,3])
-st.multiselect('Multiselect', [1,2,3])
 
 tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
 tab1.write("this is tab 1")
