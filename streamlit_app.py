e820d4135c545aa190ce87ab2057dae4823907f8
liuxi212mg <maggie_x_liu@otpp.com>
2024-09-09 16:26:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,16 @@ import numpy as np
 
 with open('style.css') as f:
     css = f.read()
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
 
+st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown(f"<style>{css}</style>",unsafe_allow_html=True)
 
 st.title('testingmg')
