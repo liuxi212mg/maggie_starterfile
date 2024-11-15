8a4ff9e1262354c684910adfc35dd1df02b85398
liuxi212mg <maggie_x_liu@otpp.com>
2024-11-15 16:57:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,41 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
-st.title('testingmg')
-st.write('Hello world!')
-
-
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
-
-
-# Show and update progress bar
-bar = st.progress(50)
-bar.progress(100)
-
-df = pd.DataFrame(
-    np.random.randn(10, 5), columns=("col %d" % i for i in range(5))
-)
-
-st.table(df)
-
-text_contents = '''This is some text'''
-st.download_button("Download some text", text_contents)
-
-
 # Dropdown selection
 options = ['+ Add New Option','Option 1', 'Option 2', 'Option 3']
 selected_option = st.selectbox('Choose an option:', options)
@@ -50,3 +15,14 @@ if selected_option == '+ Add New Option':
             st.success(f"New option '{new_option}' added!")
         else:
             st.error("Please enter a new option.")
+
+
+# Dropdown menu
+option = st.selectbox('Choose an option:', ['Option 1', 'Option 2', 'Option 3'])
+
+# Spacer to make the layout cleaner
+st.write("") 
+
+# Button for the action
+if st.button('Add New Option'):
+    st.write(f'You chose: {option}. Now you can add a new option!')
