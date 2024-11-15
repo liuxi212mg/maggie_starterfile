3c85695239a6a350946a721c12e7524217eff9ce
liuxi212mg <maggie_x_liu@otpp.com>
2024-11-15 17:27:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,36 @@
 import streamlit as st
 
-options = ['+ Add New Option', 'Option 2', 'Option 3', 'Option 4']
-options_with_placeholder = ['Select an option'] + options
-selected_option = st.selectbox('Choose an option:', options_with_placeholder)
+# Define the options for the selectbox
+options = ['Option 1', 'Option 2', 'Option 3', 'Add New Option']
 
-if selected_option == 'Select an option':
-    st.write("Please select a valid option above.")
-elif selected_option == '+ Add New Option':
+# Use a custom selectbox with 'Add New Option' as an option
+selected_option = st.selectbox('Choose an option:', options)
+
+# CSS to style the dropdown menu, emphasizing the "Add New Option" option
+st.markdown("""
+<style>
+    /* Add a custom style to the dropdown list container */
+    .stSelectbox div[data-baseweb="select"] {
+        font-size: 14px;
+        color: #333;
+    }
+    
+    /* Specifically style the "Add New Option" option */
+    .stSelectbox div[data-baseweb="select"] .option:nth-child(4) {
+        background-color: #ffcc00; /* Yellow background for "Add New Option" */
+        color: white;  /* White text color */
+        font-weight: bold; /* Make the text bold */
+    }
+    
+    /* Option hover effect */
+    .stSelectbox div[data-baseweb="select"] .option:nth-child(4):hover {
+        background-color: #e6b800; /* Darken the yellow on hover */
+    }
+</style>
+""", unsafe_allow_html=True)
+
+# Logic for selecting options
+if selected_option == 'Add New Option':
     new_item = st.text_input("Enter a new item:")
     if st.button('Add New Item'):
         if new_item:
@@ -15,5 +39,4 @@ elif selected_option == '+ Add New Option':
             st.error("Please enter a new item.")
 else:
     st.write(f'You selected: {selected_option}')
-
 
